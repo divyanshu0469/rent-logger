2189f522a772f580204d1ecee0a68e16dced38ca
divyanshu0469 <divyanshuthegreat101@gmail.com>
2025-08-22 12:02:00 +0530

    feat: add some content to home page

--- src/app/(dashboard)/home/page.tsx
+++ src/app/(dashboard)/home/page.tsx
@@ -6,6 +6,9 @@ const page = () => {
       <div className="w-full flex h-full p-6 flex-row justify-around items-center font-bold font-serif">
         Welcome To Rent logger
       </div>
+      <p>Create tenants and log rents all from a single platform</p>
+      <p>Go to Add Rent to log new rent entries</p>
+      <p>Go to View Rent to see past entires</p>
     </div>
   );
 };
